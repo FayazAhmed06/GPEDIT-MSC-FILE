2ae8d7cec8657f4cd83652657534845760b7bce4
Fayaz Ahmed <114575228+FayazHamdard@users.noreply.github.com>
2023-09-27 11:14:02 +0530

    files added

---
+++ gpedit-install.bat
@@ -0,0 +1,4 @@
+Microsoft-Windows-GroupPolicy-ClientTools-Package~…~amd64~~….mum
+Microsoft-Windows-GroupPolicy-ClientTools-Package~…~amd64~en-US~….mum
+Microsoft-Windows-GroupPolicy-ClientExtensions-Package~…~amd64~~….mum
+Microsoft-Windows-GroupPolicy-ClientExtensions-Package~…~amd64~en-US~….mum
